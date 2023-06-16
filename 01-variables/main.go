36e81ef501b7b304047658c54f940c5e51017bf1
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-06-16 20:20:19 +0900

    pointer and shadowing

--- 01-variables/main.go
+++ 01-variables/main.go
@@ -1,6 +1,9 @@
 package variables
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func basicVariables() {
 	fmt.Println("\nbasicVariables")
@@ -58,4 +61,52 @@ func Main() {
 	constVariables()
 
 	fmt.Printf("n: %v %T m: %v %T b: %v %T\n", n, n, m, m, b, b)
+
+	var ui1 uint16
+	fmt.Println(&ui1)
+
+	var ui2 uint16
+
+	fmt.Println(&ui2)
+
+	var p1 *uint16
+
+	fmt.Println(p1)
+
+	p1 = &ui1
+
+	fmt.Println(p1)
+	fmt.Println(&p1)
+	fmt.Println(*p1)
+	fmt.Println(unsafe.Sizeof(p1))
+	*p1 = 10
+	fmt.Println(ui1)
+
+	var pp1 **uint16 = &p1
+
+	// &p1 は p1 のアドレスを返す
+	// *pp1 は p1 のアドレスに格納されている値を返す
+	fmt.Println(pp1)
+	fmt.Println(&pp1)
+	fmt.Println(unsafe.Sizeof(pp1))
+
+	// *pp1 は p1 のアドレスが格納されているので、p1 のアドレスが返る
+
+	fmt.Println(*pp1)
+	fmt.Println(**pp1)
+
+	ok, result := true, "A"
+	if ok {
+		// 同じスコープ内で result を再定義している
+		// JavaScriptのletと同じ
+		result := "B"
+		println(result)
+	} else {
+		result := "C"
+		println(result)
+	}
+	println(result)
+
+	//? if の条件式で変数を宣言することができる
+
 }
