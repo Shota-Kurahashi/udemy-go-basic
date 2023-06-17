2ec1977ec7f45422e4fe06bb0173877bb7191aba
Shota-Kurahashi <Myheroes0715@gmail.com>
2023-06-17 11:28:44 +0900

    testing

---
+++ 09-unit-test/main_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	type args struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Add 3 + 5",
+			args: args{
+				x: 3,
+				y: 5,
+			},
+			want: 8,
+		},
+		{
+			name: "Add 2 + 3",
+			args: args{
+				x: 2,
+				y: 3,
+			},
+
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Add(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("Add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDivide(t *testing.T) {
+	type args struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		name string
+		args args
+		want float32
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Divide 3 / 5",
+			args: args{
+				x: 3,
+				y: 5,
+			},
+			want: 0.6,
+		}, {
+			name: "Divide 2 / 0 = 0",
+			args: args{
+				x: 2,
+				y: 0,
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Divide(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("Divide() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
